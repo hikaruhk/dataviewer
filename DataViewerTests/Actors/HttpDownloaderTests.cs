1966845a6ffbe11c2f9c64f02c2aa3aeffbe97db
aarl <hktakaoka@live.com>
2020-01-09 23:10:08 -0700

    Add test for verifying child messages are being used

--- DataViewerTests/Actors/HttpDownloaderTests.cs
+++ DataViewerTests/Actors/HttpDownloaderTests.cs
@@ -1,12 +1,19 @@
 ﻿using Akka.Actor;
 using Akka.Streams;
+using Akka.Streams.IO;
 using Akka.TestKit.NUnit;
+using AutoBogus;
+using DataViewer;
 using DataViewer.Actors;
 using DataViewer.Messages;
+using DataViewer.Messages.Enums;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using static DataViewerTests.TestingBuddies;
 
@@ -15,8 +22,9 @@ namespace DataViewerTests.Actors
     [TestFixture]
     public class HttpDownloaderTests : TestKit
     {
-        [TestCase(HttpStatusCode.OK, "https://www.abc.com/", "Content from A", Description = "Downloader returning content stream")]
-        public async Task ShouldDownloadData(
+        [TestCase(HttpStatusCode.OK, "https://www.google.com/", "Content from A", TestName = "Downloader returning content stream")]
+        [TestCase(HttpStatusCode.OK, "https://www.google.com/", "", TestName = "Downloader returning nothing from stream")]
+        public async Task ShouldDownloadHttpData(
             HttpStatusCode status,
             string url,
             string content)
@@ -42,6 +50,26 @@ namespace DataViewerTests.Actors
                 $"[{nameof(HttpDownloader)}] Stream total length was, {content.Length}",
                 result,
                 "Output message stream length differs in bytes.");
+        }
+
+        [Test]
+        public void ShouldDownloadHttpDataUsingChildren()
+        {
+            var httpClient = CreateMockedHttpClientFactory(new MockedHttpClientParameters
+            {
+                StatusCode = HttpStatusCode.OK,
+                RequestUri = new Uri("https://google.com"),
+                Content = "SomeContent"
+            });
+
+            var materializer = Sys.Materializer(namePrefix: "testMaterializer");
+            var actor = Sys.ActorOf(
+                Props.Create(() => new HttpDownloader(materializer, httpClient)),
+                "TestHttpDownloader");
+
+            actor.Tell(new HttpRequest { Uri = new Uri("https://google.com"), Action = HttpMethod.Get });
+
+            ExpectMsgAllOf<HttpResult<string>>();
         }
     }
 }
